56441bc57bfa61d302ce03af3ba813aae19990c3
Chris <chris.castro97@gmail.com>
2020-03-01 19:59:31 -0500

    added ball game

---
+++ ballGame/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+
+    public float rotationSpeed = 50;
+
+    // Camera horizontal rotation
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
