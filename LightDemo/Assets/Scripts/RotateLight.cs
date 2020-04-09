85bf2b25c2bdde8930e8865ead91cc53724558de
Chris <chris.castro97@gmail.com>
2020-04-08 21:33:26 -0400

    added lighting demo

---
+++ LightDemo/Assets/Scripts/RotateLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour
+{
+    public float speed = 30;
+    public Vector3 direction = new Vector3(1, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(direction, Time.deltaTime * speed);
+    }
+}
